8470cdfc922e56ab8d44c641422d37a170f3a67f
Piyush Jagtap <46842860+piyushjagtap22@users.noreply.github.com>
2023-08-16 20:51:34 +0530

    Sticky navbar for better UX

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
     }, [location])
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">CloudBook</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
